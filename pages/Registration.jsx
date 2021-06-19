7b36df479c48a2cd764280f9c51282daa1c5f922
yanatori <yanatori333@gmail.com>
2021-06-19 12:31:43 +0900

    add:Registrationデザイン作成

--- pages/Registration.jsx
+++ pages/Registration.jsx
@@ -7,21 +7,19 @@ export default function Registration(){
     <Layout>
       <section className={utilStyles.registration}>
         <h2>Registration</h2>
-        <label>店名</label>
-        <input placeholder="店名" />
-        <label>電話番号</label>
-        <input placeholder="電話番号" />
-        <label>住所</label>
-        <input placeholder="住所" />
-        <label for="category">カテゴリー</label>
-        <select id="category" >
-          <option value="">カテゴリー選択</option>
-          <option value="meat">肉</option>
-          <option value="fish">魚</option>
-          <option value="noodle">麺</option>
-        </select>
-      <label>メモ</label>
-      <textarea placeholder="メモ" ></textarea>
+        <div className={utilStyles.registration__box}>
+          <input placeholder="StoreName" />
+          <input placeholder="PhoneNumber" />
+          <input placeholder="StreetAddress" />
+          <select id="category" >
+            <option value="">Category</option>
+            <option value="meat">肉</option>
+            <option value="fish">魚</option>
+            <option value="noodle">麺</option>
+          </select>
+          <textarea placeholder="メモ" ></textarea>
+          <button type="button">Registration</button>
+        </div>
       </section>
     </Layout>
   )
