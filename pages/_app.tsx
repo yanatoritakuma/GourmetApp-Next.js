5588ec8a6d3cac7aac39d870fad7207e5bfbb883
yanatori <yanatori333@gmail.com>
2021-08-16 17:48:55 +0900

    fix:app.jsxをapp.tsxに変更

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,8 @@
+import { AppProps } from 'next/app'
 import '../styles/global.css'
 import store from '../app/store'
 import { Provider } from 'react-redux'
 
-
-
-function MyApp({ Component, pageProps }) {
+export default function MyApp({ Component, pageProps }: AppProps) {
   return <Provider store={store}><Component {...pageProps} /></Provider>
 }
-
-export default MyApp
