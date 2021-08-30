ab25e6a610227606ef2936aca0dd3890c2b8a653
yanatori <yanatori333@gmail.com>
2021-08-30 22:46:49 +0900

    fix:onChangeをtypescriptに修正

--- pages/Registration.tsx
+++ pages/Registration.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useMemo } from 'react';
+import React, { useState, useMemo, ChangeEvent } from 'react';
 import { useDispatch } from "react-redux";
 import Layout from "../components/Layout";
 import utilStyles from '../styles/registration.module.css';
@@ -11,7 +11,15 @@ const  Registration = () => {
   const [ streetAddress, setStreetAddress ] = useState("");
   const [note, setNote] = useState("");
   const [category, setCategory] = useState("");
-  const [photoUrl, setPhotoUrl] = useState(null);
+  const [photoUrl, setPhotoUrl] = useState<null | string>(null);
+
+  const onChangeName = (e: ChangeEvent<HTMLInputElement>) => setName(e.target.value);
+  const onChangeTel = (e: ChangeEvent<HTMLInputElement>) => setTel(e.target.value);
+  const onChangeStreetAddress = (e: ChangeEvent<HTMLInputElement>) => setStreetAddress(e.target.value);
+  const onChangeNote = (e: ChangeEvent<HTMLTextAreaElement>) => setNote(e.target.value);
+  const onChangeCategory = (e: ChangeEvent<HTMLSelectElement>) => setCategory(e.target.value);
+
+
   const dishesState = {
     name,
     tel,
@@ -19,7 +27,7 @@ const  Registration = () => {
     note,
     category,
     photoUrl,
-    id:genRandSt()
+    id: genRandSt()
   }
 
   const dispatch = useDispatch();
@@ -31,28 +39,31 @@ const  Registration = () => {
     setStreetAddress(""),
     setNote(""),
     setCategory(""),
-    setPhotoUrl("")
+    setPhotoUrl(null);
   }
-  const onChangePhoto = (e) => {
+
+  const onChangePhoto = (e:ChangeEvent<HTMLInputElement>) => {
     const photoFile = e.target.files[0];
     const photoFileUrl = URL.createObjectURL(photoFile);
     setPhotoUrl(photoFileUrl);
+    console.log("photoFile",photoFile);
   };
+
   return useMemo(() =>
     <Layout>
       <section className={utilStyles.registration}>
         <h2>Registration</h2>
         <div className={utilStyles.registration__box}>
-          <input placeholder="StoreName" value={name} onChange={(e) => {setName(e.target.value )}} />
-          <input placeholder="PhoneNumber" value={tel} onChange={(e) => {setTel(e.target.value)}} />
-          <input placeholder="StreetAddress" value={streetAddress} onChange={(e) => {setStreetAddress(e.target.value)}} />
+          <input placeholder="StoreName" value={name} onChange={onChangeName} />
+          <input placeholder="PhoneNumber" value={tel} onChange={onChangeTel} />
+          <input placeholder="StreetAddress" value={streetAddress} onChange={onChangeStreetAddress} />
           <input
             type="file"
             accept=".png, .jpg, .jpeg"
             onChange={onChangePhoto}
           />
           <img src={photoUrl} alt="プレビュー画像" />
-          <select id="category" value={category} onChange={(e) => {setCategory(e.target.value)}} >
+          <select id="category" value={category} onChange={onChangeCategory} >
             <option value="all">Category</option>
             <option value="meat">MeatDish</option>
             <option value="fish">FishDish</option>
@@ -61,7 +72,7 @@ const  Registration = () => {
             <option value="dessert">Dessert</option>
             <option value="coffee">Coffee</option>
           </select>
-          <textarea placeholder="Note" value={note} onChange={(e) => {setNote(e.target.value)}}></textarea>
+          <textarea placeholder="Note" value={note} onChange={onChangeNote}></textarea>
           <button type="button" onClick={onClickPushRegistration}>Registration</button>
         </div>
       </section>
