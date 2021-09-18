ff152f46dd0f040d8e7c8eeb2aeaf76d1a460eea
yanatori <yanatori333@gmail.com>
2021-09-18 12:21:35 +0900

    fix:tsに全ページ書き換え

--- hooks/useSelectState.tsx
+++ hooks/useSelectState.tsx
@@ -2,9 +2,12 @@ import { useState } from "react";
 
 export const useSelect = () => {
   const [selectedState, setSelectedState] = useState();
-  const onSelectState = ({ categories,categoryValue }) => {
-    const targetState = categories.find((state) => state.id === categoryValue.id);
+  const onSelectState = ({ categories, categoryValue }: any) => {
+    console.log("categories",categories)
+    console.log("categoryValue",categoryValue)
+    const targetState = categories.find((state: any) => state.id === categoryValue.id);
     setSelectedState(targetState);
+    console.log("targetState", targetState);
   }
   return { onSelectState, selectedState };
 }
