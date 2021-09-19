a7fb667dc2182bc0c27bd49699e549f0e85b183a
yanatori <yanatori333@gmail.com>
2021-09-19 11:23:48 +0900

    fix:firebase.ts修正

--- firebase.ts
+++ firebase.ts
@@ -18,28 +18,50 @@
 
 
 
-import firebase from 'firebase';
-import "firebase/app";
-import "firebase/firestore";
+// import firebase from 'firebase';
+// import "firebase/app";
+// import "firebase/firestore";
+// import "firebase/auth";
+
+// const env = process.env;
+
+// export const firebaseConfig = {
+//     apiKey: env.REACT_APP_API_KEY,
+//     authDomain: env.REACT_APP_AUTH_DOMAIN,
+//     databaseURL: env.REACT_APP_DATABASE_URL,
+//     projectId: env.REACT_APP_PROJECT_ID,
+//     storageBucket: env.REACT_APP_STORAGE_BUCKET,
+//     messagingSenderId: env.REACT_APP_MESSAGING_SENDER_ID,
+//     appId: env.REACT_APP_APP_ID,
+//     measurementId: env.REACT_APP_MEASUREMENT_ID,
+// };
+
+// export const firebaseApp = firebase.initializeApp(firebaseConfig);
+
+// export const db = firebaseApp.firestore();
+// export const auth = firebase.auth();
+
+
+// export default firebase;
+
+
+import firebase from 'firebase/compat/app'
+import 'firebase/compat/firestore'
 import "firebase/auth";
 
-const env = process.env;
 
-export const firebaseConfig = {
-    apiKey: env.REACT_APP_API_KEY,
-    authDomain: env.REACT_APP_AUTH_DOMAIN,
-    databaseURL: env.REACT_APP_DATABASE_URL,
-    projectId: env.REACT_APP_PROJECT_ID,
-    storageBucket: env.REACT_APP_STORAGE_BUCKET,
-    messagingSenderId: env.REACT_APP_MESSAGING_SENDER_ID,
-    appId: env.REACT_APP_APP_ID,
-    measurementId: env.REACT_APP_MEASUREMENT_ID,
-};
+const firebaseConfig = {
+	apiKey: process.env.REACT_APP_API_KEY,
+	authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+	databaseURL: process.env.REACT_APP_DATABASE_URL,
+	projectId: process.env.REACT_APP_PROJECT_ID,
+	storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+	messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+	appId: process.env.REACT_APP_APP_ID,
+	measurementId: process.env.REACT_APP_MEASUREMENT_ID,
+}
 
-export const firebaseApp = firebase.initializeApp(firebaseConfig);
+firebase.initializeApp(firebaseConfig);
 
-export const db = firebaseApp.firestore();
+export const db = firebase.firestore();
 export const auth = firebase.auth();
-
-
-export default firebase;
