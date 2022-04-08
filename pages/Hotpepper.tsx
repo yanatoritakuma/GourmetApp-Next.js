006af0dbb2bb69e7f06a4c9905dbe0b697d4f552
yanatori <yanatori333@gmail.com>
2022-04-08 22:08:02 +0900

    add:pageMUI

--- pages/Hotpepper.tsx
+++ pages/Hotpepper.tsx
@@ -1,9 +1,11 @@
 /** @jsxImportSource @emotion/react */
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import { css } from "@emotion/react";
 import { Layout } from "../components/Layout";
 import { HotpepperResponseType } from "../types/hotpepper";
-import { Button, IconButton, TextField, Box } from "@material-ui/core";
+import { Button, TextField, Box } from "@material-ui/core";
+import Pagination from "@mui/material/Pagination";
+import Stack from "@mui/material/Stack";
 import SearchIcon from "@mui/icons-material/Search";
 import Card from "@mui/material/Card";
 import CardContent from "@mui/material/CardContent";
@@ -16,25 +18,26 @@ const Hotpepper = () => {
   const [keyword, setKeyword] = useState("");
   const [flag, setFlag] = useState(false);
   const [initFlag, setInitFlag] = useState(true);
-  const [page, setPage] = useState({
+  const scrollTop = useRef<HTMLDivElement>(null);
+  const [pages, setPages] = useState({
     available: 0,
     start: 0,
     returned: "0",
   });
-  console.log(page);
+  console.log(pages);
 
   const onClickSearch = () => {
     setFlag(true);
     setInitFlag(true);
-    setPage({
-      ...page,
+    setPages({
+      ...pages,
       start: 0,
     });
   };
 
   useEffect(() => {
     const utf8str = encodeURIComponent(keyword);
-    const params = { start: String(page.start), keyword: utf8str };
+    const params = { start: String(pages.start), keyword: utf8str };
     const query = new URLSearchParams(params);
     if (flag) {
       const request = async () => {
@@ -50,7 +53,7 @@ const Hotpepper = () => {
 
   useEffect(() => {
     if (jsonData !== undefined && initFlag) {
-      setPage({
+      setPages({
         available: jsonData.data.results.results_available,
         start: jsonData.data.results.results_start,
         returned: jsonData.data.results.results_returned,
@@ -60,26 +63,9 @@ const Hotpepper = () => {
     }
   }, [jsonData]);
 
-  const onClickNextPage = () => {
-    setPage({
-      ...page,
-      start: page.start + 1,
-    });
-    setFlag(true);
-  };
-
-  const onClickBackPage = () => {
-    if (page.start === 1) return;
-    setPage({
-      ...page,
-      start: page.start - 1,
-    });
-    setFlag(true);
-  };
-
   return (
     <Layout>
-      <main css={hotpepper}>
+      <main css={hotpepper} ref={scrollTop}>
         <h2>Hotpepper</h2>
         <TextField
           label="検索"
@@ -112,16 +98,25 @@ const Hotpepper = () => {
               </CardContent>
             </Card>
           ))}
-          <Button
-            onClick={() => onClickNextPage()}
-            disabled={page.available < page.start * 10}
-          >
-            次のページ
-          </Button>
-          <Button onClick={() => onClickBackPage()} disabled={page.start === 1}>
-            前のページ
-          </Button>
         </section>
+        {pages.start !== 0 && pages.start !== undefined && (
+          <Stack style={{ margin: "20px auto", width: "300px" }} spacing={2}>
+            <Pagination
+              count={Math.ceil(pages.available / 10)}
+              color="secondary"
+              onChange={(e, page) => {
+                setPages({
+                  ...pages,
+                  start: page,
+                });
+                setFlag(true);
+                scrollTop?.current?.scrollIntoView();
+              }}
+              page={pages.start}
+            />
+          </Stack>
+        )}
+
         <footer style={{ margin: "10px 0", textAlign: "center" }}>
           Powered by
           <a href="http://webservice.recruit.co.jp/">
@@ -135,7 +130,7 @@ const Hotpepper = () => {
 
 const hotpepper = css`
   margin: auto;
-  padding: 20px 12px;
+  padding: 70px 12px;
   margin-top: 84px;
   padding-bottom: 40px;
   background-color: #e2dedb;
